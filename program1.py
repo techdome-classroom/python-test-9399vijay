6c83d162922a6fbe018dfc81e23265bf7ce9b19b
vijaymishra <vijaymishra8882420@gmail.com>
2024-07-26 12:56:37 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,4 +1,4 @@
-def getIslands(grid):
+def getTotalIslands(grid):
     def dfs(row, col):
         if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == 'W':
             return
